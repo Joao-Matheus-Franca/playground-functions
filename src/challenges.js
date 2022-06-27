de10fd01d49b6a5f1b59f0bb3a9e374a08214424
João Matheus Franca <matheussfranca@live.com>
2022-06-27 15:02:06 -0300

    Requisitos 01 e 02

--- src/challenges.js
+++ src/challenges.js
@@ -1,12 +1,17 @@
 // Desafio 1
-function compareTrue() {
-  // seu código aqui
+function compareTrue(parametro01, parametro02) {
+  if (parametro01 === true && parametro02 === true) {
+    return true;
+  }
+  else {
+    return false;
+  }
 }
-//Fazendo pull request
 
 // Desafio 2
-function calcArea() {
-  // seu código aqui
+function calcArea(base, altura) {
+  let area = (base * altura)/2
+  return area;
 }
 
 // Desafio 3
